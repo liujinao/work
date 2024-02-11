70bc131e3df4252820851ab906bd7324a7ac61ca
Hedy Green <73941306+liujinao@users.noreply.github.com>
2024-02-11 20:54:24 +0800

    Update BilibiliProCrack.js

--- BilibiliProCrack.js
+++ BilibiliProCrack.js
@@ -1,35 +1,3 @@
-/***********************************
-
-> 应用名称：Bilibili大会员通用破解模板[墨鱼版]
-> 软件版本：7.63.0
-> 脚本作者：ddgksf2013
-> 微信账号：墨鱼手记
-> 解锁说明：解锁VIP番剧和影视
-> 更新时间：2024-01-14
-> 通知频道：https://t.me/ddgksf2021
-> 贡献投稿：https://t.me/ddgksf2013_bot
-> 问题反馈：ddgksf2013@163.com
-> 特别提醒：如需转载请注明出处，谢谢合作！
-> 特别说明：⚠️⚠️⚠️
-          本脚本仅供学习交流使用，禁止转载售卖
-          ⚠️⚠️⚠️
-
-
-[rewrite_local]
-  
-# ～ Bilibili大会员（2024-01-14）@ddgksf2013
-^https?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player.*\.(v3|v2|v1).Play(URL|View|er).*$ url script-request-header BilibiliProCrack.js
-
-
-[mitm] 
-
-hostname=app.bilibili.com, grpc.biliapi.net,*.biliapi.net,app.bilibili.com,api.bilibili.com,api.live.bilibili.com,api.vc.bilibili.com,dataflow.biliapi.com,124.239.240.*,101.89.57.66, 218.94.210.66,240e:b1:9801:206:11:0:0:*
-
-***********************************/
-
- 
-
-
 var modifiedHeaders = $request['headers'];
 modifiedHeaders['Cookie'] = 'sid=pilun3mz; SESSDATA=0ceb0088%2C1723197108%2C27ef3221CjBz1kGh9d_ZVpiu7xWgfA5ChxmdNxhEuEi6LLUbjTtNRJXXyRa2NDdRUCvbge0fOokSVmRwTWRMREhQNWpUZEF4V3ZISFlMb1B2NzZqR1FZWWJXcmNqOWgzSnJEY25uZGR6UGs2TGxyWkxtMW1jWEp5cGJVdlJHVUVDcHFXYXFZWU1kMVZjN1ZRIIEC; bili_jct=b5409a9d5a161df91d169b0f7e5c0a77; DedeUserID=319619819; DedeUserID__ckMd5=cb83b9910b32ff93';
 modifiedHeaders['x-bili-device-bin'] = 'CAEQpN+2IhokWTQ0NUJCNEEwNzc1N0U0QjQ1NzI4RjJCOUQ1ODI1QjkxRDA1IgZpcGhvbmUqA2lvczIFcGhvbmU6BWFwcGxlQgVBcHBsZUoRaVBob25lIDEzIFBybyBNYXhSBDE1LjBqBjcuMjIuMHJAMkQ4QUIzQTc2Qzc5MzI4QjhBMDExNzY4RTE3QTg4Q0UyMDIyMDQyMDE3NTczODBERTIwODVCRDlCQzFDMjI5RniYoqLwvTA=';
